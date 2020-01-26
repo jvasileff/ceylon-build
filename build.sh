94e7072a59d16929f64c567f2dcd5c0e9d76262f
John Vasileff <john@vasileff.com>
2020-01-26 15:06:24 -0500

    Create initial build script

--- build.sh
+++ build.sh
@@ -12,21 +12,24 @@ while [ -h "$PRG" ] ; do
     fi
 done
 SAVED="`pwd`"
-cd "`dirname \"$PRG\"`/.." >/dev/null
+cd "`dirname \"$PRG\"`" > /dev/null
 APP_HOME="`pwd -P`"
-cd "$SAVED" >/dev/null
+cd "$SAVED" > /dev/null
 
 # Work from the APP_HOME directory
-cd "$APP_HOME"
+cd "$APP_HOME" > /dev/null
 
 # Setup Maven environment
-export MAVEN_HOME="$APP_HOME/apache-maven-3.5.3"
+export MAVEN_HOME="$APP_HOME/apache-maven"
 export PATH="$MAVEN_HOME/bin:$PATH"
 
+# Setup Ant environment
+export ANT_HOME="$APP_HOME/apache-ant"
+export PATH="$ANT_HOME/bin:$PATH"
+
+# Setup Ceylon environment
+export CEYLON_HOME="$APP_HOME/ceylon/dist/dist"
+export PATH="$CEYLON_HOME/bin:$PATH"
+
 # Build
-COMPONENTS="ceylon ceylon-ide-common ceylon-ide-eclipse \
-    ceylon-sdk ceylon.formatter ceylon.tool.converter.java2ceylon \
-    ceylon-ide-intellij"
-
 (cd ceylon && ant dist sdk eclipse)
-
